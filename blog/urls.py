046fb07a86688a7074ecef07418d24e2b32e2708
Jesus4890 <1730150@upv.edu.mx>
2021-01-08 09:14:24 -0600

    Modified templates to display posts from database.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.shortcuts import render
+from .models import Post
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
